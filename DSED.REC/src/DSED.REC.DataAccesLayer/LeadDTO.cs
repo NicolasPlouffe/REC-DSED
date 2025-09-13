ebbe8a0185e173474be32ef6e5f99f8c5e9d7bd6
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-09-13 15:02:03 -0400

    Depot

--- DSED.REC/src/DSED.REC.DataAccesLayer/LeadDTO.cs
+++ DSED.REC/src/DSED.REC.DataAccesLayer/LeadDTO.cs
@@ -1,11 +1,29 @@
+using DSED.REC.Entity;
 namespace DSED.REC.DataAccesLayer;
 
 public class LeadDTO
 {
     #region Properties
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
     #endregion
     
     #region CONSTRUCTOR
+
+    public LeadDTO()
+    {
+        ;
+    }
+
+    public LeadDTO(LeadEntity lead)
+    {
+        this.Id = lead.ID;
+        this.FirstName = lead.FirstName;
+        this.LastName = lead.LastName;
+        this.Email = lead.Email;
+    }
     #endregion CONSTRUCTOR
 
     #region public methods
