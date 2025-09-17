9a02c2c84f6b77ec294bcac623a7081a47db4228
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-09-16 20:45:35 -0400

    MAJ du controller et Debut Injetion Dependance

--- DSED.REC/src/DSED.REC.WebApp/Controllers/LeadController.cs
+++ DSED.REC/src/DSED.REC.WebApp/Controllers/LeadController.cs
@@ -1,30 +1,76 @@
 using Microsoft.AspNetCore.Mvc;
+using FluentValidation;
 
 using DSED.REC.Application;
 using DSED.REC.DataAccesLayer;
+using DSED.REC.Entity;
 using DSED.REC.Entity.IDepot;
 
 using DSED.REC.WebApp.Models;
 
 namespace DSED.REC.WebApp.Controllers;
 
-public class LeadController : Controller
+[ApiController]
+[Route("apié[controller]")]
+public class LeadController : ControllerBase
 {
+    private readonly ILeadDepot _leadDepot;
     private readonly LeadServiceBL _leadServiceBL;
-    private readonly ILogger<LeadController> _logger;
-
+    
     #region Constructor
 
-    public LeadController(LeadServiceBL leadServiceBL, ILogger<LeadController> logger)
+    public LeadController(ILeadDepot leadDepot,LeadServiceBL leadServiceBL)
     {
+        _leadDepot = leadDepot ?? throw new ArgumentNullException(nameof(leadDepot));
         _leadServiceBL = leadServiceBL ?? throw new ArgumentNullException(nameof(leadServiceBL));
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));;
     }
     #endregion
 
     #region CRUD
     #region Get
-    
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(500)]
+    public async Task<ActionResult<IEnumerable<LeadEntity>>> GetAllLeads()
+    {
+        try
+        {
+            var leads = await _leadDepot.GetAllLeadsAsync();
+            return Ok(leads);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex);
+        }
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<LeadEntity>> GetLeadById(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest("Lead ID cannot be empty"); 
+        }
+
+        try
+        {
+            var lead = await _leadDepot.GetLeadByIdAsync(id);
+            if (lead == null)
+            {
+                return NotFound($"Lead with {id} not found"); 
+            }
+            return Ok(lead);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+
     #endregion Get
     
     #region Post
--- DSED.REC/src/DSED.REC.WebApp/Program.cs
+++ DSED.REC/src/DSED.REC.WebApp/Program.cs
@@ -1,7 +1,34 @@
+using DSED.REC.Application;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using DSED.REC.DataAccesLayer;
+using DSED.REC.Entity;
+using DSED.REC.Entity.IDepot;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ILeadDepot, LeadDepotDepot>();
+builder.Services.AddScoped<IValidator<LeadEntity>, LeadValidator>();
+builder.Services.AddScoped<LeadServiceBL>();
+/*
+builder.Services.AddScoped<SignalR>();
+*/
+
+builder.Services.AddControllers();
+// Pour Postman
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => 
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -12,16 +39,19 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// pour permettre 
+app.UseCors();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+// app.MapHub<LeadStatsHub>("/LeadHub"); 
 
 app.Run();
